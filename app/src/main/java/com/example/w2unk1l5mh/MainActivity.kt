1650063b76e63413403dfa830246f6330c246a7f
Zytron1228 <dastelljes@outlook.com>
2022-01-28 12:38:33 -0600

    Initial commit

---
+++ app/src/main/java/com/example/w2unk1l5mh/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.w2unk1l5mh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("ඞ")
+    }
+}
